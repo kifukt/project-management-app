9b64ca75450ba8983a9acd4828c848dbcd683c63
pawel.muszynski <pawel.muszynski.95@gmail.com>
2018-04-23 23:46:12 +0200

    added default_url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,8 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
+  config.action_mailer.default_url_options = { host: 'kanban-project-management-api.herokuapp.com'}
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
